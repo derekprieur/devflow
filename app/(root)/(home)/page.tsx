e1693deb210818fa22029a27a9b9090452f7b76e
Derek Prieur <DPrieur123@gmail.com>
2023-09-26 08:25:22 -0400

    updates

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  console.log("Home");
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question">
+          <Button>Ask a Question</Button>
+        </Link>
+      </div>
+    </>
+  );
 }
