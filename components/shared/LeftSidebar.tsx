6374e87e89924e4feba6a15a3fdeafae21df48fb
Derek Prieur <DPrieur123@gmail.com>
2023-10-02 10:48:49 -0400

    updates

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -6,18 +6,10 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
 import { SignedOut, useAuth } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
 
 const LeftSidebar = () => {
   const { userId } = useAuth();
   const pathname = usePathname();
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) return null;
 
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
@@ -28,9 +20,11 @@ const LeftSidebar = () => {
             pathname === link.route;
 
           if (link.route === "/profile") {
-            link.route = `${link.route}/${userId}`;
-          } else {
-            return null;
+            if (userId) {
+              link.route = `${link.route}/${userId}`;
+            } else {
+              return null;
+            }
           }
           return (
             <Link
